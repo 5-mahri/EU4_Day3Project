fc9cd084e4d0332858b3fc004432eb2ce1548bd4
5-mahri <5-mahri@users.noreply.github.com>
2021-02-16 00:10:00 +0100

    message2

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hello Git Last Day");
         //Eu4 is excited!!!
+        //Eu4 say something nice
     }
 }
